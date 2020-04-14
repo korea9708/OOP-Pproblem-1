943e8fef4e917b4ff8cc191c1e29b7c5bbfdb2f7
Youngjun <63521629+korea9708@users.noreply.github.com>
2020-04-14 02:16:38 -0400

    Add files via upload

---
+++ Assignment1-1.java
@@ -0,0 +1,15 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		String myName="YoungJun Kim"; //declare my name
+		int number_1=3; //declare bomber 1
+		int number_2=5; //declare number 2
+		int number_3= number_1 + number_2; //add number 1 and 2 and declare 3
+		String intro="My name is "; //declare string
+		String number="and my numbers added together are "; //declare string
+		System.out.println(myName); // print my name
+		System.out.println(intro + myName + number + number_3);
+	}
+
+}
